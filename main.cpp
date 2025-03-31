98574007b341f0e88976400e8f81f73ea75bb6e7
Stasys Veitas <stasys.veitas@mif.stud.vu.lt>
2025-03-31 12:51:51 +0300

    Added assets with images to the repository. Adjusted main for presentation

--- main.cpp
+++ main.cpp
@@ -2,8 +2,29 @@
 #include "UserInput.h"
 #include "Room.h"
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
 int main() {
+    std::string name_of_a_character = "";
+
+    std::cout << "Enter the name of your character: ";
+    std::cin >> name_of_a_character;
+
+    std::string command = "python sendData.py " + name_of_a_character;
+    int status = std::system(command.c_str());
+
+    if (status == 0) {
+        std::cout << "Successfully inserted " << name_of_a_character << std::endl;
+    }
+
+    if (status == 1) {
+        std::cout << "Error executing Python script" << std::endl;
+    }
+
+    if (status == 2) {
+        std::cout << "Error: The name already exists in the database" << std::endl;
+    }
 	Room room1(1, false, true);
 	Renderer renderer(room1.getWidth(), room1.getHeight(), room1.getTileSize());
 	UserInput input;
